a240176ea306cb5b6f1f843dfafe2c2a9aa9c8d4
Esraa <esraadahy@gmail.com>
2019-05-05 12:33:40 +0200

    completed the modal.

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+class Modal {
+  constructor() {
+      this.openModalBtn = $('.open-modal');
+      this.modal = $('.modal');
+      this.closeModalBtn = $('.modal__close');
+      this.events();
+  }
+
+  events() {
+      this.openModalBtn.click(this.openModal.bind(this));
+      this.closeModalBtn.click(this.closeModal.bind(this));
+
+      $(document).keyup(this.keyPressHandler.bind(this));
+  }
+
+  openModal() {
+    this.modal.addClass('modal--is-visible');
+    return false;
+  }
+  closeModal() {
+    this.modal.removeClass('modal--is-visible');
+  }
+  keyPressHandler(e) {
+      if(e.keycode == 27) {
+          this.closeModal();
+      }
+  }
+  
+}
+
+export default Modal;
